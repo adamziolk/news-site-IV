c3c536adfcf6f491ea28342f119ef7ed47e4dbee
adamziolk <adam.lawrence.ziolkowski@gmail.com>
2021-08-10 20:39:48 -0600

    Updates

--- src/components/AppNav/AppNav.js
+++ src/components/AppNav/AppNav.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Navbar } from 'reactstrap';
 import navItems from '../../config/Sections.json';
 import { Link } from 'react-router-dom';
+import { fetchArticlesBySection } from '../../api/ArticlesAPI';
 
 
 class AppNav extends Component {
@@ -17,18 +18,18 @@ class AppNav extends Component {
       }
     }
 
-    // componentDidUpdate = async (prevProps) => {
-    //   if (this.props.sectionID !== prevProps.sectionID) {
-    //     try {
-    //       const articlesJson = await fetchArticlesBySection(this.props.match.params.sectionID);
-    //       this.setState({ articles: articlesJson });
-    //     } catch (e) {
-    //       console.error('error fetching articles: ', e);
-    //     }
-    //   }
-    // }
+    componentDidUpdate = async (prevProps) => {
+      console.log("CDU")
+      try {
+        const articlesJson = await fetchArticlesBySection(this.props.match.params.sectionID);
+        this.setState({ articles: articlesJson });
+      } catch (e) {
+        console.error('error fetching articles: ', e);
+      }
+    }
 
   render() {
+    console.log("Props", this.props)
     return (
       <Navbar color="light">
         {navItems.map((navItem) =>
